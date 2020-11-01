2858c6f612b17df3520107c0bbd9ab7d826779ab
Daniel-bin <daniel.coughlan@outlook.ie>
2020-11-01 16:20:30 +0000

    3 files modified

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,12 +2,12 @@ import { FlopListComponent } from "./flop-list/flop-list.component";
 import { WatchListComponent } from "./watch-list/watch-list.component";
 
 export const routingTable = [
-  { path: "", redirectTo: "flops", fullMatch: "full" },
-  { path: "flop", redirectTo: "flops", fullMatch: "full" },
-  { path: "movies", redirectTo: "flops", fullMatch: "full" },
-  { path: "favorites", redirectTo: "watchlist", fullMatch: "full" },
-  { path: "favs", redirectTo: "watchlist", fullMatch: "full" },
-  { path: "watch", redirectTo: "watchlist", fullMatch: "full" },
+  { path: "", redirectTo: "flops", pathMatch: "full" },
+  { path: "flop", redirectTo: "flops", pathMatch: "full" },
+  { path: "movies", redirectTo: "flops", pathMatch: "full" },
+  { path: "favorites", redirectTo: "watchlist", pathMatch: "full" },
+  { path: "favs", redirectTo: "watchlist", pathMatch: "full" },
+  { path: "watch", redirectTo: "watchlist", pathMatch: "full" },
   { path: "flops", component: FlopListComponent },
   { path: "watchlist", component: WatchListComponent }
 ];
